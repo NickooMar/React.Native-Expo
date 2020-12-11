ae9413b46bc64a925e34b0cccce1a9c445276d7c
Nico <nicoo.marsili@gmail.com>
2020-12-11 12:55:22 -0300

    Config and dotenv

---
+++ database/config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+const config = {
+  apiKey: "AIzaSyDw6lDKzigT5SLjmdcgSP8FhNHRWsqVhEY",
+  authDomain: "react-native-firebase-a12ea.firebaseapp.com",
+  databaseURL: "https://react-native-firebase-a12ea.firebaseio.com",
+  projectId: "react-native-firebase-a12ea",
+  storageBucket: "react-native-firebase-a12ea.appspot.com",
+  messagingSenderId: "424100050043",
+  appId: "1:424100050043:web:8547d48c6b4ea37c101ede",
+};
+
+module.exports = config
--- database/firebase.js
+++ database/firebase.js
@@ -1,16 +1,10 @@
 import firebase from "firebase";
 
+const config = require('./config')
+
 import "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDw6lDKzigT5SLjmdcgSP8FhNHRWsqVhEY",
-  authDomain: "react-native-firebase-a12ea.firebaseapp.com",
-  databaseURL: "https://react-native-firebase-a12ea.firebaseio.com",
-  projectId: "react-native-firebase-a12ea",
-  storageBucket: "react-native-firebase-a12ea.appspot.com",
-  messagingSenderId: "424100050043",
-  appId: "1:424100050043:web:8547d48c6b4ea37c101ede",
-};
+var firebaseConfig = config;
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
